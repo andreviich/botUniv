bf850f1763140d78edc3c53a4f85ae2b9fff8a0a
Andreeich <andronyuk.andrey@gmail.com>
2020-12-06 01:48:00 +0300

    '06.12.2020'

--- pex.py
+++ pex.py
@@ -1,93 +1,113 @@
 from pandas import read_csv, DataFrame, set_option
-set_option('display.max_rows', 1000)
-print('Загружаю группы... ')
+import settings
+from telebot import types
+import telebot
+import time
+set_option('display.max_rows', settings.MAX_ROWS)
+bot = telebot.TeleBot(settings.TOKEN)
+
+# print('Загружаю группы... ')
 groups = read_csv('groups.csv', sep=";")
-print('Загружаю результаты... ')
+# print('Загружаю результаты... ')
 results = read_csv('results.csv', sep=";")
-print('Загружаю студентов... ')
+# print('Загружаю студентов... ')
 students = read_csv('students.csv', sep=";")
-print('Загружаю предметы... ')
+# print('Загружаю предметы... ')
 subjects = read_csv('subjects.csv', sep=";")
-print('Загружаю преподавателей... ')
+# print('Загружаю преподавателей... ')
 teachers = read_csv('teachers.csv', sep=";")
-print('Готово! Данные загружены!')
+# print('Готово! Данные загружены!')
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
 
-print("""
-Для получения студентов по наименованию группы введите команду 'студенты' и название группы в верхнем или нижнем регистре. Например: 'студенты ПИ1-1'
-""")
-print("""
-Для получения преподавателей по наименованию группы введите команду 'преподаватели' и название группы в верхнем или нижнем регистре. Например: 'преподаватели ПИ1-1'
-""")
-print("""
-Для получения списка группы у определенного преподавателя введите команду 'группы' и фамилию преподавателя в именительном падеже. Например: 'группы Милованов'
-""")
-print("""
-Для получения оценок конкретного студента введите команду 'оценки', фамилию и имя студента в именительном падеже. Например: 'оценки Бирюков Максим'
-""")
-print("""
-Для получения среднего балла всех студентов у конкретного преподавателя введите команду 'средний балл' и фамилию преподавателя в именительном падеже. Например: 'средний балл Милованов'
-""")
-
-print("""
-Для для того, чтобы узнать, ведет ли данный преподаватель у конкретной группы, введите "ведёт" ,  фамилию преподавателя в именительном падеже и название группы. Например: 'ведёт Милованов ПИ1-1'
-""")
-print("""
-Для для того, чтобы узнать все оценки по конкретному предмету, введите "оценки по предмету" и название предмета. Например: 'оценки по предмету философия'
-""")
-print("""
-Для для того, чтобы узнать все оценки у конкретного преподавателя, введите "оценки преподавателя" и фамилию преподавателя в именительном падеже. Например: 'оценки преподавателя Милованов'
-""")
-print("""
-Для для того, чтобы узнать все оценки у конкретного преподавателя, введите "оценки преподавателя" и фамилию преподавателя в именительном падеже. Например: 'оценки преподавателя Милованов'
-""")
-def tryAgain(message):
-	print(message)
-def OutMessage(message):
-	print(message)
-while True:
-	comm = input('Введите команду... ').upper()
-	comm = comm.split()
+	bot.send_message(
+		message.chat.id, 'Привет! Добро пожаловать в бот!'
+	)
+# print("""
+# Для получения студентов по наименованию группы введите команду 'студенты' и название группы в верхнем или нижнем регистре. Например: 'студенты ПИ1-1'
+# """)
+# print("""
+# Для получения преподавателей по наименованию группы введите команду 'преподаватели' и название группы в верхнем или нижнем регистре. Например: 'преподаватели ПИ1-1'
+# """)
+# print("""
+# Для получения списка группы у определенного преподавателя введите команду 'группы' и фамилию преподавателя в именительном падеже. Например: 'группы Милованов'
+# """)
+# print("""
+# Для получения оценок конкретного студента введите команду 'оценки', фамилию и имя студента в именительном падеже. Например: 'оценки Бирюков Максим'
+# """)
+# print("""
+# Для получения среднего балла всех студентов у конкретного преподавателя введите команду 'средний балл' и фамилию преподавателя в именительном падеже. Например: 'средний балл Милованов'
+# """)
+#
+# print("""
+# Для для того, чтобы узнать, ведет ли данный преподаватель у конкретной группы, введите "ведёт" ,  фамилию преподавателя в именительном падеже и название группы. Например: 'ведёт Милованов ПИ1-1'
+# """)
+# print("""
+# Для для того, чтобы узнать все оценки по конкретному предмету, введите "оценки по предмету" и название предмета. Например: 'оценки по предмету философия'
+# """)
+# print("""
+# Для для того, чтобы узнать все оценки у конкретного преподавателя, введите "оценки преподавателя" и фамилию преподавателя в именительном падеже. Например: 'оценки преподавателя Милованов'
+# """)
+# print("""
+# Для для того, чтобы узнать все оценки у конкретного преподавателя, введите "оценки преподавателя" и фамилию преподавателя в именительном падеже. Например: 'оценки преподавателя Милованов'
+# """)
+def tryAgain(message, msg):
+	time.sleep(0.5)
+	bot.send_message(
+		message.chat.id, msg
+	)
+def OutMessage(message, msg):
+	time.sleep(0.5)
+	bot.send_message(
+		message.chat.id, msg
+	)
+@bot.message_handler(content_types=['text'])
+def usualMessage(message):
+	comm = message.text
+	# comm = input('Введите команду... ').upper()
+	comm = comm.upper().split()
 	allSubjects = subjects['subject_name'].values.tolist()
 	allTeachers = teachers['last_name'].values.tolist()
 	allGroups = groups['name'].values.tolist()
+	print(comm)
 	if len(comm) < 2 and 'КОМАНДЫ' not in comm:
-		OutMessage('Команда была введена неверно, повторите попытку. Для вывода всех команд введите команду "команды"')
+		OutMessage(message,'Команда была введена неверно, повторите попытку. Для вывода всех команд введите команду "команды"')
 	if 'СТУДЕНТЫ' in comm:
 		group = comm[1]
 		def getAllStudents(group):
-			try:	
+			try:
 				id_group = int(groups['id'].where(groups['name'] == group).dropna())
 			except:
-				tryAgain('Группа не найдена')
+				tryAgain(message,'Группа не найдена')
 				return
 			studentsOfThisGroup = students[['last_name', 'first_name']].where(students['group_id'] == id_group).dropna()
-			OutMessage(f'Студенты группы {group}:')
-			OutMessage(studentsOfThisGroup.to_string(index=False, header=['Фамилия', 'Имя']))
+			OutMessage(message,f'Студенты группы {group}:')
+			OutMessage(message,studentsOfThisGroup.to_string(index=False, header=['Фамилия', 'Имя'],justify="left"))
 		getAllStudents(group)
 	if 'ПРЕПОДАВАТЕЛИ' in comm:
 		group = comm[1]
-		OutMessage(f'Преподаватели группы {group}:')
+		OutMessage(message,f'Преподаватели группы {group}:')
 		def getAllTeachers(group):
 			try:
 				id_group = int(groups['id'].where(groups['name'] == group).dropna())
 			except:
-				tryAgain('Группа не найдена')
+				tryAgain(message,'Группа не найдена')
 				return
 			firstStudentOfThisGroup = students[['id']].where(students['group_id'] == id_group).dropna().iloc[0]
 			idFirstStudentOfThisGroup= round(float(firstStudentOfThisGroup.to_string(index=False, header=False)))
 			idTeachersOfThisGroup = results['teacher_id'].where(results['student_id'] == idFirstStudentOfThisGroup).dropna().astype('int32').values.tolist()
 			TeachersOfThisGroup = teachers[['last_name', 'first_name', 'middle_name']].where(teachers['id'].isin(idTeachersOfThisGroup)).dropna()
 			TeachersOfThisGroup = TeachersOfThisGroup.to_string(index=False, header=False)
-			OutMessage(TeachersOfThisGroup)
+			OutMessage(message,TeachersOfThisGroup)
 		getAllTeachers(group)
 	if 'ГРУППЫ' in comm:
 		teacher = comm[1].lower().capitalize()
-		OutMessage(f'Вывод групп преподавателя {teacher}')
+		OutMessage(message,f'Вывод групп преподавателя {teacher}')
 		def grps(teacher):
 			try:
 				idTeacher = teachers['id'].where(teachers['last_name'] == teacher).dropna().astype('int32')[0]
 			except:
-				tryAgain('Преподаватель не найден')
+				tryAgain(message,'Преподаватель не найден')
 				return
 			idsStudents = results['student_id'].where(results['teacher_id'] == idTeacher).dropna().astype('int32').values.tolist()
 			idsGroup = students['group_id'].where(students['id'].isin(idsStudents)).dropna().astype('int32').values.tolist()
@@ -97,8 +117,8 @@ while True:
 				if id not in idsGroup_undublicated:
 					idsGroup_undublicated.append(id)
 			idsGroup_undublicated.sort()
-			namesOfGroups = groups['name'].where(groups['id'].isin(idsGroup_undublicated)).dropna().to_string(index=False, header=False) 
-			OutMessage(namesOfGroups)
+			namesOfGroups = groups['name'].where(groups['id'].isin(idsGroup_undublicated)).dropna().to_string(index=False, header=False)
+			OutMessage(message,namesOfGroups)
 		grps(teacher)
 	if 'ОЦЕНКИ' in comm and len(comm)==3 and not 'ПРЕПОДАВАТЕЛЯ' in comm:
 		first_name = comm[2]
@@ -106,27 +126,30 @@ while True:
 		first_name = first_name.lower().capitalize()
 		last_name = last_name.lower().capitalize()
 		def getAllPoints(first_name, last_name):
-			try:	
+			try:
 				idStudent = students['id'].where((students['last_name'] == last_name) & (students['first_name'] == first_name)).dropna().astype('int32').values.tolist()[0]
-			except: 
-				tryAgain('Студент не найден')
+			except Exception as e:
+				# raise(e)
+				tryAgain(message,'Студент не найден')
 				return
 			resStudent = results[[ 'subject','att1', 'att2', 'exam', 'total']].where(results['student_id'] == idStudent).dropna().astype('int32')
-			resStudent = subjects.merge(resStudent, left_on="id", right_on="subject")[['subject_name', 'att1', 'att2', 'exam', 'total']]
-			outputResStudent = resStudent.to_string(index=False, header=['Предмет', 'Первая аттестация', 'Вторая аттестация', 'Экзамен', 'Тотал'])
-			OutMessage(outputResStudent)
+			resStudent = subjects.merge(resStudent, left_on="id", right_on="subject")[['subject_name', 'total']]
+			outputResStudent = resStudent.to_string(index=False, header=['Предмет','Тотал'], justify="center")
+			# OutMessage(message,outputResStudent)
+			print(outputResStudent)
+			bot.send_message(message.chat.id, outputResStudent, parse_mode = "Markdown")
 		getAllPoints(first_name, last_name)
-	if 'СРЕДНИЙ' in comm and len(comm) == 3: 
+	if 'СРЕДНИЙ' in comm and len(comm) == 3:
 		teacher = comm[2].lower().capitalize()
 		def average(teacher):
-			try:	
+			try:
 				idTeacher = teachers['id'].where(teachers['last_name'] == teacher).dropna().astype('int32')[0]
 			except:
-				tryAgain('Преподаватель не найден')
+				tryAgain(message,'Преподаватель не найден')
 				return
 			allPoints = results['total'].where(results['teacher_id'] == idTeacher).dropna().astype('int32').values.tolist()
 			average = sum(allPoints)/len(allPoints)
-			OutMessage(average)
+			OutMessage(message,average)
 		average(teacher)
 	if 'ВЕДЕТ' in comm and len(comm) ==3 or 'ВЕДЁТ' in comm and len(comm) ==3:
 		group = comm[2]
@@ -135,24 +158,24 @@ while True:
 			try:
 				idTeacher = teachers['id'].where(teachers['last_name'] == teacher).dropna().astype('int32')[0]
 			except:
-				tryAgain('Преподаватель не найден')
+				tryAgain(message,'Преподаватель не найден')
 				return
 			try:
-				OutMessage(group)
+				OutMessage(message,group)
 				id_group = groups['id'].where(groups['name'] == group).dropna().astype('int32').values.tolist()[0]
 			except Exception as e:
-				tryAgain('Данная группа не обнаружена в списке')
-				return 
+				tryAgain(message,'Данная группа не обнаружена в списке')
+				return
 			try:
 				idsStudents = results['student_id'].where(results['teacher_id'] == idTeacher).dropna().astype('int32').values.tolist()[0]
 			except:
-				tryAgain('У данного преподавателя студенты не найдены')
+				tryAgain(message,'У данного преподавателя студенты не найдены')
 				return
 			idsTeachers = results['teacher_id'].where(results['student_id'] == idsStudents).dropna().astype('int32').values.tolist()
 			if idTeacher in idsTeachers:
-				OutMessage(f'{teacher} ведёт у {group}')
+				OutMessage(message,f'{teacher} ведёт у {group}')
 			else:
-				OutMessage(f'{teacher} не ведёт у {group}')
+				OutMessage(message,f'{teacher} не ведёт у {group}')
 		isGroupEducatedByTeacher(group, teacher)
 	if 'ОЦЕНКИ' in comm and len(comm)==3 and 'ПРЕПОДАВАТЕЛЯ' in comm:
 		teacher = comm[2].lower().capitalize()
@@ -160,10 +183,10 @@ while True:
 			try:
 				idTeacher = teachers['id'].where(teachers['last_name'] == teacher).dropna().astype('int32')[0]
 			except:
-				tryAgain('Преподаватель не найден')
+				tryAgain(message,'Преподаватель не найден')
 				return
 			allpoints = results[['student_id', 'total']].where(results['teacher_id'] == idTeacher).dropna().astype('int32')
-			OutMessage(allpoints)
+			OutMessage(message,allpoints)
 		getAllPointsOfTeacher(teacher)
 	if 'ОЦЕНКИ' in comm and 'ПО' in comm and 'ПРЕДМЕТУ' in comm:
 		subject = comm[3:]
@@ -172,7 +195,7 @@ while True:
 			try:
 				idSubject = subjects['id'].where(subjects['subject_name'] == subject).dropna().astype('int32').values.tolist()[0]
 			except:
-				tryAgain('Предмет не найден')
+				tryAgain(message,'Предмет не найден')
 				return
 			allpoints = results[['student_id', 'total']].where(results['subject'] == idSubject).dropna().astype('int32')
 		getAllPointsOfSubject(subject)
@@ -193,7 +216,7 @@ while True:
 				subjects = allPoints['subject'].values.tolist()
 				subjects = list(dict.fromkeys(subjects))
 				for sub in subjects:
-					print(f'Оценки по предмету {sub}:')
+					OutMessage(message,f'Оценки по предмету {sub}:')
 					five = 0.86
 					four = 0.67
 					three = 0.42
@@ -201,48 +224,36 @@ while True:
 					fours = allPoints[['student_id','total']].where((allPoints['subject'] == sub) & (allPoints['total']/100 < five) & (allPoints['total']/100 >= four) ).dropna().astype('int32')
 					threes = allPoints[['student_id','total']].where((allPoints['subject'] == sub)  & (allPoints['total']/100 < four) & (allPoints['total']/100 >= three)).dropna().astype('int32')
 					twos = allPoints[['student_id','total']].where((allPoints['subject'] == sub) & (allPoints['total']/100 < three)).dropna().astype('int32')
-					OutMessage('Оценка "5":')
+					OutMessage(message,'Оценка "5":')
 					if fives.empty:
-						OutMessage('Пусто')
+						OutMessage(message,'Пусто')
 					else:
-						OutMessage(fives.to_string(index=False, header=['ID студента', 'Итог'],justify="left"))
-					
-					OutMessage('Оценка "4":')
+						OutMessage(message,fives.to_string(index=False, header=['ID студента', 'Итог'],justify="left"))
+					OutMessage(message,'Оценка "4":')
 					if fours.empty:
-						OutMessage('Пусто')
+						OutMessage(message,'Пусто')
 					else:
-						OutMessage(fours.to_string(index=False, header=['ID студента', 'Итог'],justify="left"))
-					OutMessage('Оценка "3":')
+						OutMessage(message,fours.to_string(index=False, header=['ID студента', 'Итог'],justify="left"))
+					OutMessage(message,'Оценка "3":')
 					if threes.empty:
-						OutMessage('Пусто')
+						OutMessage(message,'Пусто')
 					else:
-						OutMessage(threes.to_string(index=False, header=['ID студента', 'Итог'],justify="left"))
-					OutMessage('Оценка "2":')
+						OutMessage(message,threes.to_string(index=False, header=['ID студента', 'Итог'],justify="left"))
+					OutMessage(message,'Оценка "2":')
 					if twos.empty:
-						OutMessage('Пусто')
-
+						OutMessage(message,'Пусто')
 					else:
-						OutMessage(fives.to_string(index=False, header=['ID студента', 'Итог'],justify="left"))
-						OutMessage(twos.values.tolist())
-					
+						OutMessage(message,twos.to_string(index=False, header=['ID студента', 'Итог'],justify="left"))
+
 			elif group not in allGroups and teacher in allTeachers:
-				tryAgain('Такой группы не существует')
+				tryAgain(message,'Такой группы не существует')
 				return
 			elif teacher not in allTeachers and group in groups:
-				tryAgain('Преподаватель не найден')
+				tryAgain(message,'Преподаватель не найден')
 				return
 			else:
-				tryAgain('Команда была введена неверно, повторите попытку. Для вывода всех команд введите команду "команды"')
+				tryAgain(message,'Команда была введена неверно, повторите попытку. Для вывода всех команд введите команду "команды"')
 				return
 		getAllPointsOfTeacherAndGroup(teacher, group)
-		# print(subject)
-		# def getAllPointsOfTeacher(teacher):
-		# 	try:
-		# 		idTeacher = teachers['id'].where(teachers['last_name'] == teacher).dropna().astype('int32')[0]
-		# 	except:
-		# 		tryAgain('Преподаватель не найден')
-		# 		return
-		# 	allpoints = results[['student_id', 'total', 'teacher_id']].where(results['teacher_id'] == idTeacher).dropna().astype('int32')
-		# 	print(allpoints)
-		# getAllPointsOfTeacher(teacher)
-	
+
+bot.polling(none_stop=True)
